b3b59bd0b6e87737ea4383ccaa8a7f4a9c2c343b
Yuichi Okuhama <yuokuhama@gmail.com>
2022-05-20 21:22:23 -0700

    modified content

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,18 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://yuichiokuhama.github.io',
+  title: 'YO.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Yuichi Okuhama',
+  role: 'UX Designer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/yuichiokuhama/'
   },
 }
 
@@ -65,7 +64,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'yuokuhama@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
